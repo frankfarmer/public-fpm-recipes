b5b02e17e10f020a7aaa9f6717f0eb5ce3a8ccc6
frankfarmer <frank@fandom.com>
2021-10-05 12:53:31 -0700

    new calicoctl version

--- recipes/calicoctl/recipe.rb
+++ recipes/calicoctl/recipe.rb
@@ -1,10 +1,10 @@
 class Calicoctl < FPM::Cookery::Recipe
 	homepage 'https://github.com/projectcalico/calicoctl'
-	source 'https://github.com/projectcalico/calicoctl/releases/download/v3.16.5/calicoctl'
+	source 'https://github.com/projectcalico/calicoctl/releases/download/v3.20.2/calicoctl'
 	md5 '5663fbc1bdb68f4e8c0accccbfb808c8'
 
 	name 'calicoctl'
-	version '3.16.5-wikia1'
+	version '3.20.2-wikia1'
 
 	arch 'amd64'
 	maintainer 'OPS <ops-l@wikia-inc.com>'
